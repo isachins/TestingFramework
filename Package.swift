63424113ae3839f845fb72042eb9339830b90807
Sachin Sharma <code.dev.sachin@gmail.com>
2025-02-07 16:16:12 +0530

    version 0.0.2 -> added framework

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TestingFramework",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,13 +15,10 @@ let package = Package(
             targets: ["TestingFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TestingFramework"),
-        .testTarget(
-            name: "TestingFrameworkTests",
-            dependencies: ["TestingFramework"]
-        ),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "TestingFramework",
+            path: "./Sources/TestingFramework.xcframework")
     ]
 )
