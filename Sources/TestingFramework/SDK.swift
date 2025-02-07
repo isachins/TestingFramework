98dc97132dc447d436802f03a07cbf7082ef6448
Sachin Sharma <code.dev.sachin@gmail.com>
2025-02-07 14:20:39 +0530

    first commit

---
+++ Sources/TestingFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  TestingPacakage
+//
+//  Created by Sachin Sharma on 07/02/25.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomething() {
+        print("Work in progress...")
+    }
+}
