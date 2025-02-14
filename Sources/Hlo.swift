105fbb4f76e21c6c8d5efd732e8c915244f56bec
Sachin Sharma <code.dev.sachin@gmail.com>
2025-02-14 10:36:14 +0530

    hlo function

---
+++ Sources/Hlo.swift
@@ -0,0 +1,14 @@
+//
+//  Hlo.swift
+//  TestingFramework
+//
+//  Created by Sachin Sharma on 14/02/25.
+//
+
+public class Hlo {
+    public init() {}
+    
+    public func printHlo() {
+        print("hlooooo")
+    }
+}
